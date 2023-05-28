b57315881bf11f88cf3e0bc34bda5464d46e3d60
Sharjeel Ayubi <sharjeel.ayubi.007@gmail.com>
2023-05-28 17:15:58 +0500

    Initial Commit

---
+++ bike-stations/bike_stationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bike_stationsApp.swift
+//  bike-stations
+//
+//  Created by Sharjeel Ayubi on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct bike_stationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
